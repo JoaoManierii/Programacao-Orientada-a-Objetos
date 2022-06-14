a732c2299e68ab068d677613d6f828e5f6c15de2
Joao Otavio Manieri <jotamanieri@ufu.br>
2022-06-13 22:26:08 -0300

    add and update ex3 lab 02

--- LAB02 - First Java Code/ex 3/src/Ponto.java
+++ LAB02 - First Java Code/ex 3/src/Ponto.java
@@ -1,7 +1,8 @@
 public class Ponto {
-    int x,y;
+    public float x;
+    public float y;
 
-    public Ponto(int x, int y) {
+    public Ponto(float x, float y) {
         this.x = x;
         this.y = y;
     }
@@ -10,17 +11,17 @@ public class Ponto {
         System.out.println("O ponto digitado eh ("+x+","+y+")");
     }
     public Ponto soma_pontos(Ponto i, Ponto j) {
-        int x1, y1;
+        float x1, y1;
         x1 = i.x + j.x;
         y1 = i.x + j.y;
         Ponto xt = new Ponto(x1,y1);
         return xt;
     }
     public static void soma_pontos (Ponto i, Ponto j, Ponto k) {
-        int x1, y1;
+        float x1, y1;
         x1 = i.x + j.x;
         y1 = i.x + j.y;
-        System.out.println("A soma de("+i.x+","+i.y+")com("+j.x+","+j.y+") eh("+k.x+","+k.y+")");
+       // System.out.println("A soma de("+i.x+","+i.y+")com("+j.x+","+j.y+") eh("+k.x+","+k.y+")");
         k.x = x1;
         k.y = y1;
     }
@@ -29,8 +30,43 @@ public class Ponto {
     }
 
     public void multiplica_ponto (int z) {
-            x*=z;
-            y*=z;
+        x*=z;
+        y*=z;
     }
+
+    public void inc_dir(String cord){
+        if(cord.equals("l")){
+            this.x++;
+        }
+        if(cord.equals("o")){
+            this.x--;
+        }
+        if(cord.equals("n")){
+            this.y++;
+        }
+        if(cord.equals("s")){
+            this.y--;
+        }
+    }
+
+    public void ind_diag(String cord){
+        if(cord.equals("sudeste")){
+            this.x++;
+            this.y--;
+        }
+        if(cord.equals("sudoeste")){
+            this.x--;
+            this.y--;
+        }
+        if(cord.equals("nordeste")){
+            this.x++;
+            this.y++;
+        }
+        if(cord.equals("noroeste")){
+            this.x--;
+            this.y++;
+        }
+    }
+
 }
 
