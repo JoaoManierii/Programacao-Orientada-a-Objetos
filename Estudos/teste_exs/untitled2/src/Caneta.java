d55dc2cfaf87dd30890086dcbf40470f86c7043b
Joao Otavio Manieri <jotamanieri@ufu.br>
2022-06-15 00:01:50 -0300

    add exs

---
+++ Estudos/teste_exs/untitled2/src/Caneta.java
@@ -0,0 +1,44 @@
+public class Caneta {
+    public String modelo;
+    private String cor;
+    private float ponta;
+    private boolean tampada;
+
+    public Caneta(String modelo, String cor, float ponta) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ponta = ponta;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+}
