11425d54aa07a894854a224befda7bdddbade260
Joao Otavio Manieri <jotamanieri@ufu.br>
2022-07-29 11:12:22 -0300

    att

--- Trabalho/Instituição/Contas/Poupanca.java
+++ Trabalho/Instituição/Contas/Poupanca.java
@@ -3,4 +3,7 @@ package Instituição.Contas;
 public class Poupanca extends Conta {
     // nro da conta, saldo atual, data de abertura, data da última movimentação, rendimento do mês atual.
 
+    private float rendimentoMes;
+
+    
 }
